bfe1a73051a3485dcac6b7fa58f59166db6d6f42
MarynaProkopchuk <154086940+MarynaProkopchuk@users.noreply.github.com>
2024-02-10 13:48:15 +0200

    після доопрацювання

--- dz5_2.py
+++ dz5_2.py
@@ -2,16 +2,16 @@ import re
 from typing import Callable
 
 def generator_numbers(text: str):                  # створюємо генератор
-    income_list = re.findall(r"\d+\.\d+", text)    # шукаємо дійсні числа в рядку і передаємо в список
+    income_list =[float(x) for x in (filter(lambda x: re.search(r"\d+\.\d+", x),text.split(" ")))] # шукаємо числа в рядку і передаємо в список
     for income in income_list:                     # перебираємо значення в списку
-        income = float(income)                     # перетворюємо значення з рядка в дійсне число
         yield income
 
 def sum_profit(text: str, func: Callable):         # створюємо функцію для обрахунку суми
-    income_sum = sum(generator_numbers(text))      # підрахунок суми за допомогою генератора
+    income_sum = sum(func(text))                   # підрахунок суми за допомогою генератора
     return income_sum                              # повернення результату
 
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід,"\
     " доповнений додатковими надходженнями 27.45 і 324.00 доларів."     # задаємо аргумент функції
 total_income = sum_profit(text, generator_numbers)  # викликаємо функцію обрахунку суми
 print(f"Загальний дохід: {total_income}")           # виводимо результат
+
